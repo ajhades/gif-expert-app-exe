82983c087da566263a2198aebe0341ca1b0a15e1
Alvaro Borrero <ajhades@hotmail.com>
2024-10-28 08:49:31 -0500

    loading

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,6 +8,7 @@ export const GifGrid = ({ category }) => {
   return (
     <article>
       <h3>{category}</h3>
+      {isLoading && <h2>... Cargando ...</h2>}
       <ol className="card-grid">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
