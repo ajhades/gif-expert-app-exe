25728de9b05401ab86f30ec6185ebdf623f66e7f
Alvaro Borrero <ajhades@hotmail.com>
2024-10-28 17:58:16 -0500

    test GifExpertApp

--- test/GifExpertApp.test.jsx
+++ test/GifExpertApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { GifExpertApp } from "../src/GifExpertApp";
 
 describe("Pruebas en <GifExpertApp />", () => {
@@ -7,8 +7,31 @@ describe("Pruebas en <GifExpertApp />", () => {
 
     expect(screen.getByText("GifExpertApp")).toBeTruthy();
   });
-  // test("debe agregar una nueva categoria onAddCategory", () => {
-  //   render(<GifExpertApp />);
+  test("debe agregar una nueva categoria", () => {
+    render(<GifExpertApp />);
 
-  // });
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    fireEvent.input(input, { target: { value: "Dragon Ball" } });
+    fireEvent.submit(form);
+
+    expect(screen.getByText("Dragon Ball")).toBeTruthy();
+  });
+
+  test("no debe agregar una categoria existente", () => {
+    render(<GifExpertApp />);
+
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    fireEvent.input(input, { target: { value: "Superman" } });
+    fireEvent.submit(form);
+
+    fireEvent.input(input, { target: { value: "Superman" } });
+    fireEvent.submit(form);
+
+    const items = screen.getAllByText("Superman");
+    expect(items.length).not.toBeGreaterThan(1);
+  });
 });
