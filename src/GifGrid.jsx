39aa48fa8ff54a6e4c7a1ad46e0fd6b600556025
Alvaro Borrero <ajhades@hotmail.com>
2024-10-28 08:29:22 -0500

    create component GifItem

--- src/GifGrid.jsx
+++ src/GifGrid.jsx
@@ -1,14 +1,25 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getGifs } from "./helpers/getGifts";
+import { GifItem } from "./GifItem";
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async (category) => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
   useEffect(() => {
-    getGifs(category);
+    getImages(category);
   }, []);
 
   return (
     <article>
       <h3>{category}</h3>
-      <p>{category}</p>
+      <ol className="card-grid">
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </ol>
     </article>
   );
 };
