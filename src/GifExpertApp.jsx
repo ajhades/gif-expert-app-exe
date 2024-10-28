a79c55045ed9d24d96b47d6330e00fc443912770
Alvaro Borrero <ajhades@hotmail.com>
2024-10-27 23:36:57 -0500

    prevent repeat name

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,6 +3,7 @@ import { AddCategory } from "./AddCategory";
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["Marvel", "Naruto"]);
   const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     setCategories([...categories, newCategory]);
   };
   return (
