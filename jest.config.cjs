c785a9af047553e0ff793ae8f5b83340abe4b091
Alvaro Borrero <ajhades@hotmail.com>
2025-02-22 11:10:42 -0500

    probando covertura

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,11 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   collectCoverage: true,
   coverageReporters: ["lcov", "text"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}", // Incluye todos los archivos JS/JSX en la carpeta src
+    "!src/**/*.test.{js,jsx}", // Excluye archivos de prueba
+    "!src/**/index.js" // Excluye archivos index.js si no son relevantes
+  ],
   coverageDirectory: "coverage",
   setupFiles: ['whatwg-fetch','<rootDir>/jest.setup.js'],
 };
